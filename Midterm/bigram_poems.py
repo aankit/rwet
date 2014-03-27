65a14cef45a58782aceb3873adf785223d3b7d6e
Aankit Patel <Aankit@Aankits-MacBook-Pro.local>
2014-03-27 02:35:09 -0400

    Midterm

---
+++ Midterm/bigram_poems.py
@@ -0,0 +1,15 @@
+import sys, random
+
+nfile = open(sys.argv[1])
+vfile = open(sys.argv[2])
+afile = open(sys.argv[3])
+nouns = [noun.strip() for noun in nfile] 
+verbs = [verb.strip() for verb in vfile] 
+adjs = [adj.strip() for adj in afile]
+
+for x in range(random.randint(0, 10)):
+	one = adjs[random.randint(0, len(adjs)-1)]
+	two = nouns[random.randint(0, len(nouns)-1)]
+	three = verbs[random.randint(0, len(verbs)-1)]
+	print " ".join([one, two, three])
+
